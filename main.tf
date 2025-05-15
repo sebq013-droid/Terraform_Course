2c7d853095edfeca17fc2dccd91a7091e9dd2e0e
Mike Pout <michael.pout@necsws.com>
2025-05-15 17:21:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,13 +66,14 @@ module "alb" {
 	}
 	]
 
-    http_tcp_listeners = [
+     listeners = {
+	 http_tcp_listener = 
 	{
       port     = 80
       protocol = "HTTP"
 	  target_group_index =0
     }
-	]
+	}
 	
   tags = {
     Environment = "Dev"
