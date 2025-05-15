0064c89f17e102c29fca5cd9f25b21737edd578c
Mike Pout <michael.pout@necsws.com>
2025-05-15 17:11:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
 
   
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = {
       name_prefix      = "blog-"
