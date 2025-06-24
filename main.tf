c355ae053ca9fc5c7319205d73a03ce5ec36bb1c
Mike Pout <michael.pout@necsws.com>
2025-06-24 16:52:53 +0100

    autoscaling

--- main.tf
+++ main.tf
@@ -29,18 +29,26 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  tags = {
-    Name = "Learning Terraform"
-  }
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.3.0"
+
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+  
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog)alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+
+
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
