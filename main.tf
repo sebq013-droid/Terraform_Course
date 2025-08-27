4a4fb40d1cf14c47bde7ba1933d51277eaf593c9
xartius89 <xartius@gmail.com>
2025-08-27 21:09:58 +0300

    Some SG update

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "example" {
+data "aws_vpc" "defvpc" {
   default = true
 }
 
@@ -22,19 +22,21 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  # vpc_security_group_ids = [aws_security_group.blog.id, module.xart_blog_sg.security_group_id]
-  vpc_security_group_ids = [module.xart_blog_sg.security_group_id] 
+  #vpc_security_group_ids = [module.xart_blog_secgroup.security_group_id] 
+  vpc_security_group_ids = [resource.aws_security_group.blog.id]
+
+
   tags = {
     Name = "XartTestInstance"
   }
 }
 
-module "xart_blog_sg" {
+module "xart_blog_secgroup" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.example.id
+  vpc_id = data.aws_vpc.defvpc.id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -47,7 +49,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http/https inbound. Allow everything out"
 
-  vpc_id = data.aws_vpc.example.id
+  vpc_id = data.aws_vpc.defvpc.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -64,6 +66,16 @@ resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "ssh_in" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
