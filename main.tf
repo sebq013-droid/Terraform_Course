92890abca8b04c20a0cbe84945a0e0d0d886ef30
Hillbird79 <150556162+Hillbird79@users.noreply.github.com>
2025-10-16 19:00:34 +0200

    New commit dev US WEST AWS

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1", "eu-west-2", "eu-west-3"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
