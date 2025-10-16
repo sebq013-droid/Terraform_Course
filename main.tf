408d2143ee270e1724767b3a0f0decd667385c6e
Hillbird79 <150556162+Hillbird79@users.noreply.github.com>
2025-10-16 14:40:13 +0200

    Parameterize instance type in main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "blog" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "Learning Terraform"
