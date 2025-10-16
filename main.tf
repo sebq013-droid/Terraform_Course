25ff42d03b2a4aab989598cdd48da0d5ff296d7d
Hillbird79 <150556162+Hillbird79@users.noreply.github.com>
2025-10-16 18:50:51 +0200

    New vpc dev AWS

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1"], "[eu-west-2"], ["eu-west-3"]
+  azs             = ["eu-west-1", "eu-west-2", "eu-west-3"]
 
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
