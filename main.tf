560bc5e86ebda2917bb1c650290ff52aaae40a16
shawnkahlon <shawnkahlon@gmail.com>
2025-11-06 13:30:40 -0500

    fix again

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
-  target_group_arns = module.blog_alb.target_group_arns
+
 
   image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
