40ecb9ca31d6646f0728db71104f2697cd96a799
shawnkahlon <shawnkahlon@gmail.com>
2025-11-06 13:22:14 -0500

    last fix?

--- main.tf
+++ main.tf
@@ -39,8 +39,9 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = moduel.blog_vpc.public_subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
+  target_group_arns = module.blog_alb.target_group_arns
 
   image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
