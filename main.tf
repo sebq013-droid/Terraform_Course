76e8926148a466824a35a21fb47898e658cff0f8
cesarlarah4 <cesarlarah4@gmail.com>
2025-12-10 10:58:03 -0600

    fixed name

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ module "blog_sg" {
   name= "blog_new"
 
   vpc_id =  module.blog_vpc.vpc_id
-  name="blog"
+  
   
   ingress_rules =["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
