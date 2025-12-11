0da1ba360c4e5d741fee29472fb6ce1477f2d13a
cesarlarah4 <cesarlarah4@gmail.com>
2025-12-11 11:05:17 -0600

    fix subnets

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "alb" {
 
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
-  subnets = [module.blog_sg.public_subnets]
+  subnets = module.blog_vpc.public_subnets
 
   # Security Group
   security_group_ingress_rules = [module.blog_sg.security_group_id]
