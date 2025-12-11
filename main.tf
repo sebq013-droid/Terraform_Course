be9d66a285984bb53848bdb5fb1fe1af2aa9e7e1
cesarlarah4 <cesarlarah4@gmail.com>
2025-12-11 11:35:59 -0600

    delete target from alb

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_groups_arns  = module.blog_alb.target_groups_arns
+  target_groups_arns  = module.blog_alb.target_groups
   security_groups  =  [module.blog_sg.security_group_id]
   
   image_id           = data.aws_ami.app_ami.id
@@ -72,16 +72,7 @@ module "blog_alb" {
       }
     }   
   }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
+  
 
   tags = {
     Environment = "Dev"
