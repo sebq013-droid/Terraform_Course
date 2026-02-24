02a5ef84d12bc5a7bd4d9b0bef9ec017a82a11fe
sebq013-droid <sebq013@gmail.com>
2026-02-24 11:20:23 +0000

    Autoscaling3

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ module "blog_autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = module.blog.vpc_public_subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
 
   launch_template_name = "blog"
   security_groups    = [module.blog_sg.security_group_id]
