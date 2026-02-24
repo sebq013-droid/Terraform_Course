164d3f3dc0bec49f9af8e8bf21fc718005a09223
sebq013-droid <sebq013@gmail.com>
2026-02-24 10:40:37 +0000

    Fix issues 3

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ module "blog_alb" {
       port     = 80
       protocol = "HTTP"
       forward = {
-        target_group_artn = aws_lb_target_group.blog.arn
+        target_group_arn = aws_lb_target_group.blog.arn
       }
     }
   }
