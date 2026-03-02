f760ee0aa9bed253e849d02bc0a468435b1396c0
sebq013-droid <sebq013@gmail.com>
2026-03-02 11:54:10 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type   = object  ({
+  name   = string
+  powner = string
+})
+
+  default  = {
+    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner  = "979382823631" # Bitnami
+ }
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type = object ({
+  name           = string
+  network_prefix = string
+})
+default = {
+name             = "dev"
+network_prefix   = "10.0"
+  } 
+}
+
+Variable "min_size"{
+  description = "minimum number of instances in the ASG"
+  default = 1
+}
+
+Variable "max_size"{
+  description = "minimum number of instances in the ASG"
+  default = 2
+}
+ 
